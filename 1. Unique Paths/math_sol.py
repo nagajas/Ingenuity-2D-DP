31662a5cd2f4281bae9c75ec009361a97f0cb97a
nagajas <123821782+nagajas@users.noreply.github.com>
2024-07-03 03:52:54 +0530

    Edit code: Correct code

--- 1. Unique Paths/math_sol.py
+++ 1. Unique Paths/math_sol.py
@@ -2,8 +2,8 @@ def combinations(n,r):
     pi = 1
     for i in range(1,r+1):
         pi *= (n-i+1)
-        pi //= i
-    return pi
+        pi /= i
+    return int(pi)
 
 def uniquePaths(m, n):
     return combinations(m+n-2, m-1)
