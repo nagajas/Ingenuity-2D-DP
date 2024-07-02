e27a49d0690f4b785f9cc2b22ed8ff7084173eb7
nagajas <123821782+nagajas@users.noreply.github.com>
2024-07-02 22:05:12 +0530

    Update Code: Concise code

--- 1. Unique Paths/bottom_up.cpp
+++ 1. Unique Paths/bottom_up.cpp
@@ -4,9 +4,7 @@ using namespace std;
 int unique_paths(int n, int m){
     // Time Complexity: O(n*m)
     // Space Complexity: O(n*m)
-    vector<vector<int>> dp(m+1, vector<int>(n+1)); // Extra row and column buffer to handle overflow
-    for(int i=0; i<=m; i++) dp[i][0] = 0; // Set overflow row and column to zero
-    for(int i=0; i<=n; i++) dp[0][i] = 0;
+    vector<vector<int>> dp(m+1, vector<int>(n+1,0)); // Extra row and column buffers are set to zero to handle overflow
     dp[1][1] = 1; // Base Case: If there's only one cell, only 1 path exists
     for(int i=1; i<=m; i++){
         for(int j=1; j<=n; j++){
